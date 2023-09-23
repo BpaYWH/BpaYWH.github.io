21f3472b64e4ee4e8d896d8235f1606b81aeabe0
BpaYWH <yauwinghongbpa@gmail.com>
2023-09-23 13:12:04 -0400

    UPDATE: skills sections

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,9 +1,12 @@
-import { MouseEvent } from "react";
+"use client";
+
+import { Fragment, MouseEvent, useState } from "react";
 import Image from "next/image";
 
-import { techStacksList } from "@/utils/config";
+import { frontendStacks, backendStacks, langStacks, techStacksList } from "@/utils/config";
 
 function About() {
+   const [isXVisible, setXVisible] = useState<boolean>(false);
 
    const stopPropagation = (e: MouseEvent) => {
       e.preventDefault();
@@ -21,16 +24,17 @@ function About() {
                         Hi! I'm Henry Yau, a Full Stack Developer with experience creating web applications.
                      </p>
                      <p className="sm:text-sm">
-                        I'm actively seeking opportunities where I can leverage my expertise in JavaScript, React, and Laravel to drive innovation and deliver exceptional user experiences. 
+                        I'm actively seeking opportunities where I can leverage my expertise (mainly in JavaScript / TypeScript) to drive innovation and deliver exceptional user experiences. 
                      </p>
                   </div>
                </div>
 
-               <div id="skill" className="flex flex-col gap-4">
+               <div id="skill" className="flex flex-col space-y-4">
                   <h3 className="text-2xl md:text-xl">Tech Stacks</h3>
-                  <div className="flex flex-wrap gap-4">
-                     {
-                        techStacksList.map(item => (
+                  <Fragment key="language">
+                     <h4 className="text-lg">Languages</h4>
+                     <div id="" className="flex flex-wrap gap-x-4 gap-y-4">
+                        {   langStacks.map(item => (
                            <Image 
                            src={item.icon} 
                            alt={item.name}
@@ -39,9 +43,62 @@ function About() {
                            height={28} 
                            className="transition hover:scale-[1.2]"
                            />
-                        ))
-                     }
-                  </div>
+                           ))
+                        }
+                     </div>
+                  </Fragment>
+
+                  <Fragment key="frontend">
+                     <h4 className="text-lg">Frontend</h4>
+                     <div className="flex flex-wrap gap-x-4 gap-y-4">
+                        {
+                           frontendStacks.map(item => (
+                              <Image 
+                              src={item.icon} 
+                              alt={item.name}
+                              title={item.name}
+                              width={28} 
+                              height={28}
+                              className="transition hover:scale-[1.2]"
+                              />
+                           ))
+                        }
+                     </div>
+                  </Fragment>
+
+                  <Fragment key="backend">
+                     <h4 className="text-lg">Backend</h4>
+                     <div className="flex flex-wrap gap-x-4 gap-y-4">
+                        {   backendStacks.map(item => (
+                              <Image 
+                              src={item.icon} 
+                              alt={item.name}
+                              title={item.name}
+                              width={28} 
+                              height={28} 
+                              className="transition hover:scale-[1.2]"
+                              />
+                           ))
+                        }
+                     </div>
+                  </Fragment>
+
+                  <Fragment key="tech">
+                     <h4 className="text-lg">Technologies</h4>
+                     <div id="tech" className="flex flex-wrap gap-x-4 gap-y-4">
+                        {   techStacksList.map(item => (
+                              <Image 
+                              src={item.icon} 
+                              alt={item.name}
+                              title={item.name}
+                              width={28} 
+                              height={28} 
+                              className="transition hover:scale-[1.2]"
+                              />
+                           ))
+                        }
+                     </div>
+                  </Fragment>
                </div>
 
                <div id="contact" className="flex gap-4 justify-end">
@@ -52,7 +109,15 @@ function About() {
                      <Image src="/assets/image/icon/In-Blue-48.png" alt="LinkedIn" width={24} height={24} />
                   </a>
                   <a href="https://github.com/BpaYWH" title="Github" className="transition hover:scale-110" target="_blank" rel="noreferrer">
-                     <Image src="/assets/image/icon/github-mark.svg" alt="Github" width={24} height={24} />
+                     <Image src="/assets/image/icon/tech/github-mark.svg" alt="Github" width={24} height={24} />
+                  </a>
+                  <a href="https://twitter.com/_HenryYau_" title="X" className="transition hover:scale-110" target="_blank" rel="noreferrer" onMouseEnter={() => {setXVisible(true)}} onMouseLeave={() => {setXVisible(false)}}>
+                     {
+                        isXVisible ?
+                        <Image src="/assets/image/icon/x.svg" alt="X" width={24} height={24} />
+                        :
+                        <Image src="/assets/image/icon/twitter-color.svg" alt="Twitter" width={24} height={24} />
+                     }
                   </a>
                </div>
             </div>
--- utils/config.tsx
+++ utils/config.tsx
@@ -40,7 +40,13 @@ export const LabPageList: TPage[] = [
    //    path: "expenses",
    //    description: "Track your expenses",
    //    image: "",
-   // }
+   // },
+   {
+      name: "Bid Room",
+      path: "bid-room",
+      description: "",
+      image: ""
+   }
 ];
 
 export const ProjectPageList: TPage[] = [
@@ -64,37 +70,165 @@ export const ProjectPageList: TPage[] = [
    }
 ];
 
-export const techStacksList: TTech[] = [
+export const langStacks: TTech[] = [
    {
       name: "JavaScript",
-      icon: "/assets/image/icon/javascript-color.svg"
+      icon: "/assets/image/icon/lang/javascript-color.svg"
    },
    {
       name: "TypeScript",
-      icon: "/assets/image/icon/typescript-color.svg"
-   },
-   {
-      name: "React",
-      icon: "/assets/image/icon/react-color.svg"
-   },
-   {
-      name: "Next.js",
-      icon: "/assets/image/icon/nextdotjs-color.svg"
-   },
-   {
-      name: "TailwindCSS",
-      icon: "/assets/image/icon/tailwindcss-color.svg"
+      icon: "/assets/image/icon/lang/typescript-color.svg"
    },
    {
       name: "HTML5",
-      icon: "/assets/image/icon/html5-color.svg"
+      icon: "/assets/image/icon/lang/html5-color.svg"
    },
    {
       name: "CSS3",
-      icon: "/assets/image/icon/css3-color.svg"
+      icon: "/assets/image/icon/lang/css3-color.svg"
    },
    {
+      name: "C#",
+      icon: "/assets/image/icon/lang/csharp-color.svg"
+   },
+   {
+      name: "C++",
+      icon: "/assets/image/icon/lang/cplusplus-color.svg"
+   },
+   {
+      name: "Python",
+      icon: "/assets/image/icon/lang/python-color.svg"
+   },
+   {
+      name: "PHP",
+      icon: "/assets/image/icon/lang/php-color.svg"
+   },
+   {
+      name: "Go",
+      icon: "/assets/image/icon/lang/go-color.svg"
+   }
+];
+
+export const frontendStacks: TTech[] = [
+   {
+      name: "React.JS",
+      icon: "/assets/image/icon/frontend/react-color.svg"
+   },
+   {
+      name: "Next.JS",
+      icon: "/assets/image/icon/frontend/nextdotjs-color.svg"
+   },
+   {
+      name: "TailwindCSS",
+      icon: "/assets/image/icon/frontend/tailwindcss-color.svg"
+   },
+   {
+      name: "Sass",
+      icon: "/assets/image/icon/frontend/sass-color.svg"
+   },
+
+   {
+      name: "Material UI",
+      icon: "/assets/image/icon/frontend/mui-color.svg"
+   },
+   {
+      name: "Bootstrap",
+      icon: "/assets/image/icon/frontend/bootstrap-color.svg"
+   },
+
+   {
+      name: "Framer Motion",
+      icon: "/assets/image/icon/frontend/framer-color.svg"
+   },
+   {
+      name: "RadixUI",
+      icon: "/assets/image/icon/frontend/radixui-color.svg"
+   },
+   {
+      name: "Ant Design",
+      icon: "/assets/image/icon/frontend/antdesign-color.svg"
+   },
+
+];
+
+export const backendStacks: TTech[] = [
+   {
+      name: "Node.js",
+      icon: "/assets/image/icon/backend/nodedotjs-color.svg"
+   },
+   {
+      name: ".Net",
+      icon: "/assets/image/icon/backend/dotnet-color.svg"
+   },
+   {
+      name: "Laravel",
+      icon: "/assets/image/icon/backend/laravel-color.svg"
+   },
+   {
+      name: "PostgreSQL",
+      icon: "/assets/image/icon/backend/postgresql-color.svg"
+   },
+   {
+      name: "MongoDB",
+      icon: "/assets/image/icon/backend/mongodb-color.svg"
+   },
+   {
+      name: "Prisma",
+      icon: "/assets/image/icon/backend/prisma-color.svg"
+   },
+   {
+      name: "GraphQL",
+      icon: "/assets/image/icon/backend/graphql-color.svg"
+   },
+   {
+      name: "Docker",
+      icon: "/assets/image/icon/backend/docker-color.svg"
+   }
+];
+
+export const techStacksList: TTech[] = [
+   {
       name: "Amazon Web Service",
-      icon: "/assets/image/icon/amazonaws-color.svg"
+      icon: "/assets/image/icon/tech/amazonaws-color.svg"
+   },
+      {
+      name: "Adobe XD",
+      icon: "/assets/image/icon/tech/adobexd-color.svg"
+   },
+   {
+      name: "Webpack",
+      icon: "/assets/image/icon/tech/webpack-color.svg"
+   },
+   {
+      name: "Vite",
+      icon: "/assets/image/icon/tech/vite-color.svg"
+   },
+   {
+      name: "Figma",
+      icon: "/assets/image/icon/tech/figma-color.svg"
+   },
+   {
+      name: "Bun",
+      icon: "/assets/image/icon/tech/bun-color.svg"
+   },
+   {
+      name: "Jest",
+      icon: "/assets/image/icon/tech/jest-color.svg"
+   },
+   {
+      name: "Jira",
+      icon: "/assets/image/icon/tech/jira-color.svg"
+   },
+   {
+      name: "npm",
+      icon: "/assets/image/icon/tech/npm-color.svg"
+   },
+   {
+      name: "Github Actions",
+      icon: "/assets/image/icon/tech/githubactions-color.svg"
+   },
+   {
+      name: "Git",
+      icon: "/assets/image/icon/tech/github-mark.svg"
    }
 ];
